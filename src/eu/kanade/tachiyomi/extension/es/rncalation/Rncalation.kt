f5b9f497d23961b6d7c29af9e12eb385c49f71e5
BYJoanHD <76027917+BYJoanHD@users.noreply.github.com>
2026-06-12 02:08:34 -0400

    Create Rncalation.kt

---
+++ src/eu/kanade/tachiyomi/extension/es/rncalation/Rncalation.kt
@@ -0,0 +1,22 @@
+package eu.kanade.tachiyomi.extension.es.rncalation
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Rncalation : ParsedHttpSource() {
+
+    override val name = "RNCALATION"
+    override val baseUrl = "https://rncalation.online"
+    override val lang = "es"
+    override val
